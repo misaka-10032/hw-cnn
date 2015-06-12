correct svm/softmax mistakes

assignment1/cs231n/classifiers/linear_svm.py
@@ -134,7 +134,8 @@ def svm_loss_vectorized(W, X, y, reg):
 #  print 'adding ddW takes %fs' % (toc - tic)
 
   dW /= X.shape[1]
-  dW += reg * np.sum(W)
+  # dW += reg * np.sum(W)
+  dW += reg * W
 
   #############################################################################
   #                             END OF YOUR CODE                              #

assignment1/cs231n/classifiers/softmax.py
@@ -37,7 +37,8 @@ def softmax_loss_naive(W, X, y, reg):
   loss = np.sum(-scores_y + log_sums)
 
   loss /= X.shape[1]
-  loss += .5 * np.sum(W * W)
+  # loss += .5 * np.sum(W * W)
+  loss += .5 * reg * np.sum(W * W)
 
   # dW
   for i in xrange(X.shape[1]):